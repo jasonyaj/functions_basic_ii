50f7941d4cb8a619fcac07f43e247f761e569e57
jasonyaj <jason.yaj@gmail.com>
2023-04-02 19:59:02 -0500

    updated with alternative solutions

--- functions_basic_ii.py
+++ functions_basic_ii.py
@@ -7,10 +7,10 @@ def countdown(num):
 print(countdown(5))
 
 # 2. Print and Return
-def print_and_return(num1, num2):
-    print(num1)
-    return num2
-print(print_and_return(2, 4))
+def print_and_return(arr):
+    print(arr[0])
+    return arr[1]
+print(print_and_return([1,2]))
 
 # 3. First Plus Length
 def first_plus_length(arr):
@@ -32,6 +32,21 @@ def values_greater_than_second(arr):
     return newList
 print(values_greater_than_second([5,2,3,2,1,4]))
 
+# solution from Coding Dojo
+def values_greater_than_second(list):
+    if len(list) < 2:
+        return False
+    output = []
+    for i in range(0,len(list)):
+        if list[i] > list[1]:
+            output.append(list[i])
+    print(len(output))
+    return output
+
+
+print(values_greater_than_second([5,2,3,2,1,4]))
+print(values_greater_than_second([3]))
+
 # 5. This Length, That Value
 def length_and_value(size, value):
     newList = []
@@ -39,3 +54,13 @@ def length_and_value(size, value):
         newList.append(value)
     return newList
 print(length_and_value(6, 2))
+
+# solution from Coding Dojo
+def length_and_value(size,value):
+    output = []
+    for i in range(0, size):
+        output.append(value)
+    return output
+
+print(length_and_value(4,7))
+print(length_and_value(6,2))
